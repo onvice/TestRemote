7cd0ecdd96cf0d8f8c4e5ce7a5b72de179728f1f
philou <toto@gmail.com>
2022-01-21 16:04:12 +0100

    integration du projet

---
+++ src/main/java/com/formation/integcontinue/hello_world_app/App.java
@@ -0,0 +1,13 @@
+package com.formation.integcontinue.hello_world_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
